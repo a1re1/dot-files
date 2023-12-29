cd12ea532ac421b8e8212e06c7b1ad6ea5a28d51
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-12-29 07:39:52 +0000

    add nvim alias

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@ then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
 fi
 
+
 # set up powerline theme
 sed -i "/OSH_THEME=/c\OSH_THEME=\"powerline\"" ~/.bashrc
+
 
 # if nvim isnt installed yet install it
 if ! command -v nvim &> /dev/null
@@ -23,10 +25,16 @@ then
     sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 fi
 
+
 # set up aliases
 if ! grep "alias reload=" ~/.bashrc &> /dev/null
 then
     echo "alias reload='source ~/.bashrc'" >> ~/.bashrc
 fi
+if ! grep "alias vim=" ~/.bashrc &> /dev/null
+then
+    echo "alias vim='nvim'" >> ~/.bashrc
+fi
+
 
 echo "✅ Done! Please open a new terminal to finish setup."
