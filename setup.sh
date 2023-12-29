bccbcb724248ad402b392febb8a0881aa6efba6c
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-12-29 04:56:02 +0000

    add

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@
 # if oh-my-bash
 if [ ! -d "/home/codespace/.oh-my-bash" ]
 then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-    sed -i "/OSH_THEME=/c\OSH_THEME=\"powerline\"" ~/.bashrc
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" > /dev/null
 fi
+
+# set up powerline theme
+sed -i "/OSH_THEME=/c\OSH_THEME=\"powerline\"" ~/.bashrc
 
 # if nvim isnt installed yet install it
 if ! command -v nvim &> /dev/null
@@ -19,7 +21,7 @@ then
     ./squashfs-root/AppRun --version
     sudo mv squashfs-root /
     sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-    sudo apt-get install neovim
+    sudo apt-get install neovim > /dev/null
 fi
 
 # set up aliases
