8896996bb50eedd6672e35b964a425341319ca32
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-12-30 00:50:57 +0000

    elixir

--- setup.sh
+++ setup.sh
@@ -40,12 +40,13 @@ then
   sudo apt update
   sudo apt install elixir erlang-dev erlang-xmerl
   sudo apt-get install inotify-tools
+  sudo apt install postgresql
+  sudo systemctl start postgresql
+  #install pheonix
+  mix local.hex
+  mix archive.install hex phx_new
   echo "export PATH='$PATH:/usr/bin/elixir/bin'" >> ~/.bashrc
 fi
-
-#install pheonix
-mix local.hex
-mix archive.install hex phx_new
 
 # set up aliases
 if ! grep "alias reload=" ~/.bashrc &> /dev/null
